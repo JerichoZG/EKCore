26bf98b87cd714c0a2ef5f3673b34de997b5ee15
JerichoZG <jyy909523@gmail.com>
2021-01-11 14:44:06 +0800

    Update

--- Combat/alRaidCD.lua
+++ Combat/alRaidCD.lua
@@ -14,59 +14,53 @@ local texture = "Interface\\TargetingFrame\\UI-StatusBar"
 local show = {
 	raid = true,
 	party = true,
-	arena = true,
+	arena = true
 	--none = true,
 }
 -- Config end
 
 local spells = {
 	--群疗
-	[740]    = 180,  -- 宁静
-	[115310] = 180,  -- 还魂
-	[64843]  = 180,  -- 神圣赞美诗
-	[108280] = 180,  -- 奶潮
-	[15286]  = 180,  -- 吸血鬼拥抱
-	
+	[740] = 180, -- 宁静
+	[115310] = 180, -- 还魂
+	[64843] = 180, -- 神圣赞美诗
+	[108280] = 180, -- 奶潮
+	[15286] = 180, -- 吸血鬼拥抱
 	-- 群体减伤
-	[51052] = 120,  -- 反魔法领域
-	[31821] = 180,  -- 光环掌握
-	[62618] = 180,  -- 真言术: 障
-	[98008] = 180,  -- 灵魂链接图腾
-	[97462] = 180,  -- 集结呐喊
-	
+	[51052] = 120, -- 反魔法领域
+	[31821] = 180, -- 光环掌握
+	[62618] = 180, -- 真言术: 障
+	[98008] = 180, -- 灵魂链接图腾
+	[97462] = 180, -- 集结呐喊
 	-- 单体减伤
 	[116849] = 120, -- 氣繭護體
 	[633] = 600, -- 圣疗
 	[6940] = 120, -- 牺牲
 	[33206] = 180, -- 痛苦压制
 	[47788] = 180, -- 守護聖靈
-	
 	-- 战术技能
-	[106898] = 120,  --豹奔
-	[1022]   = 300,	   -- 保護聖禦
-	
+	[106898] = 120, --豹奔
+	[1022] = 300, -- 保護聖禦
 	-- 战复(非首领战)
-	[20484] = 600,	-- 复生
-	[61999] = 600,	-- 复活盟友
-	[20707] = 600,	-- 灵魂石复活
+	[20484] = 600, -- 复生
+	[61999] = 600, -- 复活盟友
+	[20707] = 600, -- 灵魂石复活
 	[126393] = 600, -- 永恒守护者
-	
 	-- 其他
-	[32182] = 300,	-- 英勇
-	[2825] = 300,	-- 嗜血
-	[80353] = 300,	-- 时间扭曲
-	[90355] = 300,	-- 远古狂乱
-	[29166] = 180,	-- 激活
-	
-	[16190] = 180,  --潮汐
-	[115213] = 180,	-- 慈悲庇护
+	[32182] = 300, -- 英勇
+	[2825] = 300, -- 嗜血
+	[80353] = 300, -- 时间扭曲
+	[90355] = 300, -- 远古狂乱
+	[29166] = 180, -- 激活
+	[16190] = 180, --潮汐
+	[115213] = 180 -- 慈悲庇护
 	--[133] = 180, --测试
-	
 }
 
 local cfg = {}
 
-local filter = COMBATLOG_OBJECT_AFFILIATION_RAID + COMBATLOG_OBJECT_AFFILIATION_PARTY + COMBATLOG_OBJECT_AFFILIATION_MINE
+local filter =
+	COMBATLOG_OBJECT_AFFILIATION_RAID + COMBATLOG_OBJECT_AFFILIATION_PARTY + COMBATLOG_OBJECT_AFFILIATION_MINE
 local band = bit.band
 local sformat = string.format
 local floor = math.floor
@@ -76,8 +70,9 @@ local inBossCombat = nil
 
 local backdrop = {
 	bgFile = [=[Interface\ChatFrame\ChatFrameBackground]=],
-	edgeFile = [=[Interface\ChatFrame\ChatFrameBackground]=], edgeSize = 1,
-	insets = {top = 0, left = 0, bottom = 0, right = 0},
+	edgeFile = [=[Interface\ChatFrame\ChatFrameBackground]=],
+	edgeSize = 1,
+	insets = {top = 0, left = 0, bottom = 0, right = 0}
 }
 
 local bars = {}
@@ -85,34 +80,36 @@ local Ressesbars = {}
 local anchorframe = CreateFrame("Frame", "RaidCD", UIParent)
 anchorframe:SetSize(width, height)
 anchorframe:SetPoint(anchor, x, y)
-if UIMovableFrames then tinsert(UIMovableFrames, anchorframe) end
+if UIMovableFrames then
+	tinsert(UIMovableFrames, anchorframe)
+end
 
 local FormatTime = function(time)
 	if time >= 60 then
-		return sformat('%.2d:%.2d', floor(time / 60), time % 60)
+		return sformat("%.2d:%.2d", floor(time / 60), time % 60)
 	else
-		return sformat('%.2d', time)
+		return sformat("%.2d", time)
 	end
 end
 
 local CreateFS = CreateFS or function(frame)
-	local fstring = frame:CreateFontString(nil, 'OVERLAY', 'GameFontHighlight')
-	fstring:SetFont(font, font_size, font_style)
-	fstring:SetShadowColor(0, 0, 0, 1)
-	fstring:SetShadowOffset(0, 0)
-	return fstring
-end
+		local fstring = frame:CreateFontString(nil, "OVERLAY", "GameFontHighlight")
+		fstring:SetFont(font, font_size, font_style)
+		fstring:SetShadowColor(0, 0, 0, 1)
+		fstring:SetShadowOffset(0, 0)
+		return fstring
+	end
 
 local CreateBG = CreateBG or function(parent)
-	local bg = CreateFrame("Frame", nil, parent)
-	bg:SetPoint("TOPLEFT", parent, "TOPLEFT", -1, 1)
-	bg:SetPoint("BOTTOMRIGHT", parent, "BOTTOMRIGHT", 1, -1)
-	bg:SetFrameStrata("LOW")
-	bg:SetBackdrop(backdrop)
-	bg:SetBackdropColor(unpack(backdrop_color))
-	bg:SetBackdropBorderColor(unpack(border_color))
-	return bg
-end
+		local bg = CreateFrame("Frame", nil, parent)
+		bg:SetPoint("TOPLEFT", parent, "TOPLEFT", -1, 1)
+		bg:SetPoint("BOTTOMRIGHT", parent, "BOTTOMRIGHT", 1, -1)
+		bg:SetFrameStrata("LOW")
+		bg:SetBackdrop(backdrop)
+		bg:SetBackdropColor(unpack(backdrop_color))
+		bg:SetBackdropBorderColor(unpack(border_color))
+		return bg
+	end
 
 local UpdatePositions = function()
 	if charges and Ressesbars[1] then
@@ -123,7 +120,7 @@ local UpdatePositions = function()
 			if i == 1 then
 				bars[i]:SetPoint("TOPLEFT", Ressesbars[1], "BOTTOMLEFT", 0, -spacing)
 			else
-				bars[i]:SetPoint("TOPLEFT", bars[i-1], "BOTTOMLEFT", 0, -spacing)
+				bars[i]:SetPoint("TOPLEFT", bars[i - 1], "BOTTOMLEFT", 0, -spacing)
 			end
 			bars[i].id = i
 		end
@@ -133,10 +130,10 @@ local UpdatePositions = function()
 			if i == 1 then
 				bars[i]:SetPoint("TOPLEFT", anchorframe, 0, 0)
 			else
-				bars[i]:SetPoint("TOPLEFT", bars[i-1], "BOTTOMLEFT", 0, -spacing)
+				bars[i]:SetPoint("TOPLEFT", bars[i - 1], "BOTTOMLEFT", 0, -spacing)
 			end
 			bars[i].id = i
-		end	
+		end
 	end
 end
 
@@ -162,7 +159,7 @@ local UpdateCharges = function(bar)
 	end
 	if curCharges ~= currentNumResses then
 		currentNumResses = curCharges
-		bar.left:SetText(bar.name.." : "..currentNumResses)
+		bar.left:SetText(bar.name .. " : " .. currentNumResses)
 	end
 end
 
@@ -194,25 +191,25 @@ end
 local OnMouseDown = function(self, button)
 	if button == "LeftButton" then
 		if self.isResses then
-		  if IsInRaid() then
-			  SendChatMessage(sformat("Battle Res: %d; Next: %s", currentNumResses, self.right:GetText()), "RAID")
-		  elseif IsInGroup() then
-			  SendChatMessage(sformat("Battle Res: %d; Next: %s", currentNumResses, self.right:GetText()), "PARTY")
-		  elseif IsInInstance() then
-			  SendChatMessage(sformat("Battle Res: %d; Next: %s", currentNumResses, self.right:GetText()), "INSTANCE")
-		  else
-			  SendChatMessage(sformat("Battle Res: %d; Next: %s", currentNumResses, self.right:GetText()), "SAY")
-		  end	
+			if IsInRaid() then
+				SendChatMessage(sformat("Battle Res: %d; Next: %s", currentNumResses, self.right:GetText()), "RAID")
+			elseif IsInGroup() then
+				SendChatMessage(sformat("Battle Res: %d; Next: %s", currentNumResses, self.right:GetText()), "PARTY")
+			elseif IsInInstance() then
+				SendChatMessage(sformat("Battle Res: %d; Next: %s", currentNumResses, self.right:GetText()), "INSTANCE")
+			else
+				SendChatMessage(sformat("Battle Res: %d; Next: %s", currentNumResses, self.right:GetText()), "SAY")
+			end
 		else
-		  if IsInRaid() then
-			  SendChatMessage(sformat("Cooldown %s %s: %s", self.left:GetText(), self.spell, self.right:GetText()), "RAID")
-		  elseif IsInGroup() then
-			  SendChatMessage(sformat("Cooldown %s %s: %s", self.left:GetText(), self.spell, self.right:GetText()), "PARTY")
-		  elseif IsInInstance() then
-			  SendChatMessage(sformat("Cooldown %s %s: %s", self.left:GetText(), self.spell, self.right:GetText()), "INSTANCE")
-		  else
-			  SendChatMessage(sformat("Cooldown %s %s: %s", self.left:GetText(), self.spell, self.right:GetText()), "SAY")
-		  end		
+			if IsInRaid() then
+				SendChatMessage(sformat("Cooldown %s %s: %s", self.left:GetText(), self.spell, self.right:GetText()), "RAID")
+			elseif IsInGroup() then
+				SendChatMessage(sformat("Cooldown %s %s: %s", self.left:GetText(), self.spell, self.right:GetText()), "PARTY")
+			elseif IsInInstance() then
+				SendChatMessage(sformat("Cooldown %s %s: %s", self.left:GetText(), self.spell, self.right:GetText()), "INSTANCE")
+			else
+				SendChatMessage(sformat("Cooldown %s %s: %s", self.left:GetText(), self.spell, self.right:GetText()), "SAY")
+			end
 		end
 	elseif button == "RightButton" then
 		StopTimer(self)
@@ -228,16 +225,16 @@ local CreateBar = function()
 	bar.icon:SetPoint("LEFT", 0, 0)
 	bar.status:SetPoint("BOTTOMLEFT", bar.icon, "BOTTOMRIGHT", 3, 0)
 	bar.status:SetPoint("BOTTOMRIGHT", 0, 0)
-	bar.status:SetHeight(height/2.4)
+	bar.status:SetHeight(height / 2.4)
 	bar.status:SetStatusBarTexture(texture)
 	bar.status:SetMinMaxValues(0, 100)
-	bar.status:SetFrameLevel(bar:GetFrameLevel()-1)
+	bar.status:SetFrameLevel(bar:GetFrameLevel() - 1)
 	bar.left = CreateFS(bar)
-	bar.left:SetPoint('LEFT', bar.status, 2, 5)
-	bar.left:SetJustifyH('LEFT')
+	bar.left:SetPoint("LEFT", bar.status, 2, 5)
+	bar.left:SetJustifyH("LEFT")
 	bar.right = CreateFS(bar)
-	bar.right:SetPoint('RIGHT', bar.status, -2, 5)
-	bar.right:SetJustifyH('RIGHT')
+	bar.right:SetPoint("RIGHT", bar.status, -2, 5)
+	bar.right:SetJustifyH("RIGHT")
 	CreateBG(bar.icon)
 	CreateBG(bar.status)
 	return bar
@@ -265,7 +262,7 @@ local StartTimer = function(name, spellId)
 		local curCharges, _, _, duration = GetSpellCharges(20484)
 		currentNumResses = curCharges
 		bar.endTime = GetTime() + duration
-		bar.left:SetText(name.." : "..curCharges)
+		bar.left:SetText(name .. " : " .. curCharges)
 		bar.right:SetText(FormatTime(duration))
 		bar.isResses = true
 		color = RAID_CLASS_COLORS[select(2, UnitClass("player"))]
@@ -343,8 +340,13 @@ local OnEvent = function(self, event, ...)
 	end
 	if event == "COMBAT_LOG_EVENT_UNFILTERED" then
 		local timestamp, eventType, _, sourceGUID, sourceName, sourceFlags = ...
-		if band(sourceFlags, filter) == 0 then return end
-		if (eventType == "SPELL_RESURRECT" and not charges) or eventType == "SPELL_CAST_SUCCESS" or eventType == "SPELL_AURA_APPLIED" then
+		if band(sourceFlags, filter) == 0 then
+			return
+		end
+		if
+			(eventType == "SPELL_RESURRECT" and not charges) or eventType == "SPELL_CAST_SUCCESS" or
+				eventType == "SPELL_AURA_APPLIED"
+		 then
 			local spellId = select(12, ...)
 			if sourceName then
 				sourceName = sourceName:gsub("-.+", "")
@@ -366,14 +368,14 @@ local OnEvent = function(self, event, ...)
 end
 
 local addon = CreateFrame("frame")
-addon:SetScript('OnEvent', OnEvent)
+addon:SetScript("OnEvent", OnEvent)
 addon:RegisterEvent("PLAYER_ENTERING_WORLD")
 addon:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 addon:RegisterEvent("ZONE_CHANGED_NEW_AREA")
 
-SlashCmdList["RaidCD"] = function(msg) 
-	StartTimer(UnitName('player'), 20484)
-	StartTimer(UnitName('player'), 740)
-	StartTimer(UnitName('player'), 20707)
+SlashCmdList["RaidCD"] = function(msg)
+	StartTimer(UnitName("player"), 20484)
+	StartTimer(UnitName("player"), 740)
+	StartTimer(UnitName("player"), 20707)
 end
 SLASH_RaidCD1 = "/raidcd"
